4ce519c864a26e9d6025e4643cb77a6550891ed6
Sumit <sumit1github@gmail.com>
2025-07-18 20:37:42 +0530

    chnage

--- react_js/src/config.js
+++ react_js/src/config.js
@@ -1,9 +1,9 @@
-// export const config = {
-//     api_host: "https://thebhumiputra.com",
-// };
-
 export const config = {
-    api_host: "http://localhost:8000",
+    api_host: "https://thebhumiputra.com",
 };
+
+// export const config = {
+//     api_host: "http://localhost:8000",
+// };
 
 
